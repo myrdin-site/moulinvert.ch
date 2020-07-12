c1d20735029110c0e546839340506632323498c4
Myrdin <myrdin@clic2000.fr>
2020-07-12 14:04:35 +0200

    Scrolling parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 function scroll() {
     var section = document.getElementById("section-two");
-    section.scrollIntoView();
+    section.scrollIntoView({behavior: 'smooth', block: 'center'});
 }
 
 document.getElementById("button-scroll").addEventListener("click", scroll);
