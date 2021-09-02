75878602bd011be117ca1d6b2d5158a868fe5c34
Mathilde <mathilde@clic2000.fr>
2021-09-02 22:14:50 +0200

    add NPM config files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+      enabled: false, // change at true for production
+      content: ['*.html'],
+    },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
