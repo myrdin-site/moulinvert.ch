6284e6ef98a3099878a0ad73ff443d7fd6e222b4
Myrdin <myrdin@clic2000.fr>
2022-03-09 12:35:42 +0100

    Change purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  content: {
-      enabled: false, // change at true for production
-      content: ['*.html'],
-    },
+  content: [
+      '*.html',
+    ],
   media: false, // or 'media' or 'class'
   theme: {
     extend: {},
